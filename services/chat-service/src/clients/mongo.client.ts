4f1e8c7031b07020a3a6e27db49791932a52404d
Tarun222999 <tarun@levitation.co.in>
2026-02-18 23:16:17 +0530

    setup chat-service

---
+++ services/chat-service/src/clients/mongo.client.ts
@@ -0,0 +1,27 @@
+import { MongoClient } from "mongodb";
+
+import { env } from "@/config/env";
+import { logger } from "@/utils/logger";
+
+let client: MongoClient | null = null;
+
+export const getMongoClient = async (): Promise<MongoClient> => {
+    if (client) {
+        return client;
+    }
+    const mongoUrl = env.MONGO_URL
+    client = new MongoClient(mongoUrl)
+    await client.connect()
+    logger.info('Mongodb connection established')
+
+    return client
+}
+
+export const closeMongoClient = async () => {
+    if (!client) {
+        return
+    }
+    await client.close()
+    logger.info('Mongodb COnnection Closed')
+    client = null
+}
---
+++ services/chat-service/src/config/env.ts
@@ -0,0 +1,22 @@
+import "dotenv/config"
+
+import { createEnv, z } from "@chatapp/common"
+
+const envSchema = z.object({
+    NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
+    CHAT_SERVICE_PORT: z.coerce.number().int().min(0).max(65_535).default(4000),
+    INTERNAL_API_TOKEN: z.string().min(16),
+    JWT_SECRET: z.string().min(32),
+    RABBITMQ_URL: z.string().url(),
+    REDIS_URL: z.string().url(),
+    MONGO_URL: z.string().url()
+})
+
+type EnvType = z.infer<typeof envSchema>;
+
+
+export const env: EnvType = createEnv(envSchema, {
+    serviceName: 'gateway-service'
+})
+
+export type Env = typeof env
